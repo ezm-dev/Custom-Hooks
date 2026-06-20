e4796d3611982bf623ec6f18d36fa1be75601b4e
ezm-dev <ezm.sabry@gmail.com>
2026-06-20 12:24:44 +1000

    feat: implement useToggle hook for managing boolean state

--- src/hooks/useToggle.jsx
+++ src/hooks/useToggle.jsx
@@ -1,2 +1,13 @@
 
+import React from "react";
+
+export default function useToggle(){
+    const[on,setOn] =React.useState(false)
+
+    function toggle(){
+        setOn(prev=>!prev)
+    
+    }
+    return [on,toggle]
+}
 
